d58ca3d6f9e7691b570137f6502994e12745fec5
sponge <sponge@d8d.org>
2015-12-11 06:46:40 -0600

    build batch

---
+++ build.bat
@@ -0,0 +1,9 @@
+rmdir /S /Q dist
+mkdir dist
+"c:\Program Files\7-Zip\7z.exe" a -tzip dist/game.love ./*.lua base/* game/*
+xcopy "c:\Program Files (x86)\LOVE\*.dll" dist
+copy /b "c:\Program Files (x86)\LOVE\love.exe"+dist\game.love dist\game.exe
+del dist\game.love
+cd dist
+"c:\Program Files\7-Zip\7z.exe" a -tzip game.zip *
+cd ..
