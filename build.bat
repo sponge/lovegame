cac7e05e1df2863da06d9ed28aea381f7d99610e
sponge <sponge@d8d.org>
2015-12-24 14:17:58 -0600

    update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 rmdir /S /Q dist
 mkdir dist
-"c:\Program Files\7-Zip\7z.exe" a -tzip dist/game.love ./*.lua base/* game/*
+"c:\Program Files\7-Zip\7z.exe" a -tzip dist/game.love ./*.lua base/* game/* Quickie/*
 xcopy "c:\Program Files (x86)\LOVE\*.dll" dist
 copy /b "c:\Program Files (x86)\LOVE\love.exe"+dist\game.love dist\game.exe
+move dist\game.love c:\dropbox\game.love
 del dist\game.love
 cd dist
 "c:\Program Files\7-Zip\7z.exe" a -tzip game.zip *
